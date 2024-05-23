b8de8720884842cc9b3e8a42870ac6cb64c758e6
BacaV <nikolajevtic3405@gmail.com>
2024-05-23 11:41:32 +0200

    Finished

--- app.js
+++ app.js
@@ -1,15 +1,45 @@
+//////////////// zadatak 1
 
-    const imageSelect = document.getElementById('imageSelect');
-    const displayImage = document.getElementById('displayImage');
 
-    imageSelect.addEventListener('change', () => {
-        const selectedValue = imageSelect.value;
-        
-        if (selectedValue) {
-            displayImage.src = selectedValue;
-            displayImage.style.display = 'block';
-        } else {
-            displayImage.src = '';
-            displayImage.style.display = 'none';
-        }
-    });
+function zadatak1(){
+
+    let inputs = document.querySelectorAll('input[name=dest]');
+
+    for(let i = 0; i< inputs.length; i++){
+        document.querySelectorAll('input[name=dest]')[i].addEventListener('click',
+            function(){
+                let value = this.value;
+                document.getElementById("slika").src = "/images/" + value;
+            }
+    )
+}
+}
+
+
+//////////////// zadatak 2
+
+function zadatak2(){
+
+    let check = document.querySelector("input[name=prevoz]").checked;
+    
+    if(check){
+        document.getElementById('polje1').style = "display:block;"
+    } else {
+        document.getElementById('polje1').style = "display:none;"
+    }
+}
+
+//////////////// zadatak 3
+
+function zadatak3(){
+
+    let checkbox = document.querySelector('input[name=komentar]');
+
+    if(checkbox.checked){
+        document.querySelector('textarea[name=kom]').disabled = false ;
+    } else {
+        document.querySelector('textarea[name=kom]').disabled = true ;
+    }
+   
+
+}
